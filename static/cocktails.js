8cf22bd250583dd2672045c05d8aaea4a9c54d5e
sade2 <cedricsman@gmail.com>
2022-01-13 16:53:48 -0500

    merge again

--- static/cocktails.js
+++ static/cocktails.js
@@ -1,5 +1,4 @@
 function search_drinks(drinkName) {
-
     var that = this
     $.ajax({
         type: "GET",
@@ -15,14 +14,18 @@ function search_drinks(drinkName) {
 
 function search_drinks_based_on_found(drink) {
     var that = this
-    $.ajax({
-        type: "GET",
-        url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName,
-        timeout: 600000,
-        success: function (data) {
-            console.log(data);
-            that.drinks = data
-        },
-      });
+
+    for (var i = 0; i < that.drinks.length; i++) { 
+        $.ajax({
+            type: "GET",
+            url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName,
+            timeout: 600000,
+            success: function (data) {
+                console.log(data);
+                that.drinks[i] = data
+            },
+          });
+    }
+    
 }
 
