b6a23d1d86c26a899e0de8ddfc42eb1954a65705
Hanyuan Cui <cui.532@osu.edu>
2022-01-13 16:49:53 -0500

    updated cocktail.js and index.html

--- static/cocktails.js
+++ static/cocktails.js
@@ -6,21 +6,17 @@ function button_click() {
 function search_drinks(drinkName) {
     let drinksArray = null;
 
+    var that = this
     $.ajax({
         type: "GET",
         url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName,
         timeout: 600000,
         success: function (data) {
             console.log(data);
-            that.receipts = data
-            that.receiptsUpdate()
-            drinksArray = data
+            that.drinks = data
         },
       });
-
-
-    // let x = document.getElementsByClassName('animals');
 }
 
 
-search_drinks(userInput)
+search_drinks(button_click)
